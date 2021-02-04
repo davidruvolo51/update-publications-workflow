60d7b36277d18875d1dbbf85f9c14cfa6ea4f500
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-04 21:39:47 +0100

    restructured repo as 'package'

---
+++ dev/dev.R
@@ -0,0 +1,23 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: dev.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2021-02-04
+#' MODIFIED: 2021-02-04
+#' PURPOSE: pkg management
+#' STATUS: in.progress
+#' PACKAGES: NA
+#' COMMENTS: NA
+#'////////////////////////////////////////////////////////////////////////////
+
+#' init project
+usethis::create_project(path = ".")
+usethis::use_description(check_name = FALSE)
+usethis::use_namespace()
+
+#' pkgs
+usethis::use_package("dplyr")
+usethis::use_package("httr")
+usethis::use_package("rjson")
+usethis::use_package("rlist")
+usethis::use_package("stringr")
+usethis::use_package("lubridate")
