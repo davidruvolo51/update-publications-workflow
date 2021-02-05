da0d3018c1d3c93e7f330f88ae09ec1f2aa11ad4
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-05 18:22:39 +0100

    fixed unused arg error

--- R/update.R
+++ R/update.R
@@ -15,22 +15,22 @@ suppressPackageStartupMessages(library(dplyr))
 queries <- data.frame()
 data <- data.frame()
 tryCatch({
-    message("Loaded reference datasets\n")
+    message("Loaded reference datasets\n\t- API queries\n\t- publications")
     queries <<- read.csv("https://raw.githubusercontent.com/davidruvolo51/update-publications-workflow/main/data/api_queries.csv")
     data <<- read.csv("https://raw.githubusercontent.com/davidruvolo51/update-publications-workflow/main/data/publications.csv")
 }, error = function(error) {
-    warning("Unable to load reference datasets\n")
+    warning("Unable to load reference datasets")
 }, warning = function(warn) {
-    warning("Unable to load reference datasets\n")
+    warning("Unable to load reference datasets")
 })
 
 # Init object to write api results into
 api <- list()
 
 # Run queries to identify new publications
-message("Processing publication queries:\n")
+message("Processing publication queries:")
 if (NROW(queries$query) > 1) {
-    message("\t- binding multiple queries into an array\n")
+    message("\t- binding multiple queries into an array")
     api$ids <- unlist(
         lapply(
             X = queries$query,
@@ -40,28 +40,28 @@ if (NROW(queries$query) > 1) {
         )
     )
 } else {
-    message("\t- fetching IDs using one query\n")
+    message("\t- fetching IDs using one query")
     api$ids <- pubmed$get_ids(queries$query)
 }
 
 # remove existing IDs from api ID query list
-message("Removing existing IDs using reference data\n")
+message("Removing existing IDs using reference data")
 api$ids <- api$ids[!api$ids %in% data$uid]
 
 # fetch publication metadata (if there are new Ids)
 if (length(api$ids)) {
-    message(paste0("Pulling Metadata for new IDs:\n\t- ", api$ids, "\n"))
+    message(paste0("Pulling Metadata for new IDs: ", api$ids))
     result <- pubmed$get_metadata(
         ids = api$ids,
         delay = sample(runif(50, 0.75, 2), length(api$ids))
     )
 
     # prepare publications dataset
-    pubs <- pubmed$build_df(x = result)
+    pubs <- pubmed$build_df(data = result)
     message(paste0("Returned data dims:", dims(pubs)))
 
     # save data
-    message("Writing data\n")
+    message("Saving new publications!")
     write.csv(pubs, "data/test.csv", row.names = FALSE)
 } else {
     message("No new publications")
